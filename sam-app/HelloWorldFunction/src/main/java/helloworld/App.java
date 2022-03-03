e4ee55fdcbaadd3ecf5697de6b22b75eb66083df
Joshua Taylor <joshua.taylor@digital.cabinet-office.gov.uk>
2022-03-03 11:14:58 +0000

    Add error detecting cloudwatch alarm
    Also HelloWorldFunction always return an error. This will be reverted
    
    after PLAT-51 is completed.
    
    Issue: PLAT-51
    Co-authored-by: Adrian Wong <adrian.wong@digital.cabinet-office.gov.uk>

--- sam-app/HelloWorldFunction/src/main/java/helloworld/App.java
+++ sam-app/HelloWorldFunction/src/main/java/helloworld/App.java
@@ -30,7 +30,7 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
             String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
 
             return response
-                    .withStatusCode(200)
+                    .withStatusCode(500)
                     .withBody(output);
         } catch (IOException e) {
             return response
