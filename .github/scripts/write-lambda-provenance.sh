f05c2e6a274eac556c27bdfef57cf692002f93b9
Mike Winter <mike.winter@digital.cabinet-office.gov.uk>
2022-04-29 11:49:46 +0100

    Normalise lambda metadata case
    Metadata keys are only stored in S3 in lowercase.
    
    Issue: PLAT-72
    Co-authored-by: Joshua Taylor <joshua.taylor@digital.cabinet-office.gov.uk>

--- .github/scripts/write-lambda-provenance.sh
+++ .github/scripts/write-lambda-provenance.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 yq '.. | select(has("Type") and .Type == "AWS::Serverless::Function") | .Properties.CodeUri' cf-template.yaml \
-    | xargs -L1 -I{} aws s3 cp "{}" "{}" --metadata "Repository=$GITHUB_REPOSITORY,CommitSHA=$GITHUB_SHA"
+    | xargs -L1 -I{} aws s3 cp "{}" "{}" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA"
