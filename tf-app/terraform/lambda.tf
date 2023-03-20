f7dfa2a97be7afffe43c04d96b2e50ca5d990f08
Beca Galliano <beca.galliano@digital.cabinet-office.gov.uk>
2023-03-20 16:05:11 +0000

    Updated lambda naming to be unique

--- tf-app/terraform/lambda.tf
+++ tf-app/terraform/lambda.tf
@@ -11,14 +11,18 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+resource "random_id" "id" {
+  byte_length = 8
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  name                 = "iam_for_lambda-${random_id.id.hex}"
+  assume_role_policy   = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_lambda_function" "helloworld_lambda" {
   filename      = "../HelloWorldFunction/build/libs/HelloWorldFunction.jar"
-  function_name = "helloworld_lambda"
+  function_name = "helloworld_lambda-${random_id.id.hex}"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "helloworld.App::handleRequest"
 
