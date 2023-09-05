91bae1d9006077066e17131c1cfd6236edd6b642
Richa Misra <richa.misra@digital.cabinet-office.gov.uk>
2023-09-05 18:38:48 +0100

    Initial commit

--- sam-app3/image/app.py
+++ sam-app3/image/app.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 def hello():
     print("This is a log message")
     return "Hello World!"
+
+@app.route("/test")
+def test():
+    print("This is a test call")
+    return "Test successful!"
