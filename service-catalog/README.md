84daf6e4bc14ef76fbdee9664940381e6142bdae
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2023-10-30 13:09:21 +0000

    Started to add terraform for provisioning pipelines

---
+++ service-catalog/README.md
@@ -0,0 +1,15 @@
+Service catalog
+
+Overview
+AWS Service Catalog requires many manual steps in order to be configured.
+
+Installation/Instructions
+Deploy Portfolio AWS CloudFormation template sample for AWS Service Catalog. Deploy Product 
+AWS CloudFormation template sample for AWS Service Catalog.
+
+Issues
+### Gotchas
+
+## Cleanup
+
+## Resources
--- terraform/deployments/devplatform-development/pipeline-deploy/stacks.tf
+++ terraform/deployments/devplatform-development/pipeline-deploy/stacks.tf
@@ -7,11 +7,13 @@ data "aws_cloudformation_stack" "aws-signer" {
 
 module "service-catalog-pipeline" {
   source     = "git@github.com:alphagov/di-ipv-terraform-modules.git//secure-pipeline/deploy-pipeline"
+  template_url = "https://template-bucket-templatebucket-35qbug5k1irh.s3.eu-west-2.amazonaws.com/test-templates/sam-deploy-pipeline/PLAT-2936-template.yaml"
   stack_name = "service-catalog-pipeline"
   parameters = {
     SAMStackName               = "service-catalog-poc"
     Environment                = "dev"
     VpcStackName               = "vpc"
+    IncludePromotion           = "No"
     # AWSOrganizationId          = data.aws_organizations_organization.gds.id
     LogRetentionDays           = 7
     SigningProfileArn          = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileArn"]
