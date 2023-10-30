7046797230e2df757b26d51ad7523c788f6802ba
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2023-10-30 12:06:18 +0000

    Started to add terraform for provisioning pipelines

--- terraform/deployments/devplatform-development/pipeline-deploy/outputs.tf
+++ terraform/deployments/devplatform-development/pipeline-deploy/outputs.tf
@@ -1,11 +1,11 @@
-output "service-catalog-demo-pipeline_stack_id" {
-  value = module.service-catalog-demo-pipeline.stack_id
+output "service-catalog-pipeline_stack_id" {
+  value = module.service-catalog-pipeline.stack_id
 }
-output "service-catalog-demo-pipeline_stack_outputs" {
-  value = module.service-catalog-demo-pipeline.stack_outputs
+output "service-catalog-pipeline_stack_outputs" {
+  value = module.service-catalog-pipeline.stack_outputs
 }
-output "service-catalog-demo-pipeline_stack_tags" {
-  value = module.service-catalog-demo-pipeline.stack_tags
+output "service-catalog-pipeline_stack_tags" {
+  value = module.service-catalog-pipeline.stack_tags
 }
 
 output "gds_org_id" {
--- terraform/deployments/devplatform-development/pipeline-deploy/stacks.tf
+++ terraform/deployments/devplatform-development/pipeline-deploy/stacks.tf
@@ -2,78 +2,26 @@ data "aws_organizations_organization" "gds" {}
 
 
 data "aws_cloudformation_stack" "aws-signer" {
-  name = "aws-signer-pipeline"
+  name = "aws-signer"
 }
 
-module "dns-zones-pipeline" {
+module "service-catalog-pipeline" {
   source     = "git@github.com:alphagov/di-ipv-terraform-modules.git//secure-pipeline/deploy-pipeline"
-  stack_name = "dns-zones-pipeline"
+  stack_name = "service-catalog-pipeline"
   parameters = {
-    SAMStackName               = "dns-zones-build"
-    Environment                = "build"
+    SAMStackName               = "service-catalog-poc"
+    Environment                = "dev"
     VpcStackName               = "vpc"
-    AllowedAccounts            = "791977510340"
     # AWSOrganizationId          = data.aws_organizations_organization.gds.id
     LogRetentionDays           = 7
     SigningProfileArn          = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileArn"]
     SigningProfileVersionArn   = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileVersionArn"]
-    GitHubRepositoryName       = "di-shared-assets"
-    OneLoginRepositoryName     = "shared-assets"
+    OneLoginRepositoryName     = "devplatform-demo-sam-app"
     SlackNotificationType      = "Failures"
-    BuildNotificationStackName = "di-assets-notifications"
+    BuildNotificationStackName = "build-notifications"
   }
 
   tags_custom = {
-    System = "DI ASSETS"
-  }
-}
-
-module "upload-assets-pipeline" {
-  source     = "git@github.com:alphagov/di-ipv-terraform-modules.git//secure-pipeline/deploy-pipeline"
-  stack_name = "upload-assets-pipeline"
-  parameters = {
-    SAMStackName             = "upload-assets"
-    Environment              = "build"
-    VpcStackName             = "vpc"
-    AllowedAccounts          = "791977510340"
-    # AWSOrganizationId        = data.aws_organizations_organization.gds.id
-    LogRetentionDays         = 7
-    SigningProfileArn        = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileArn"]
-    SigningProfileVersionArn = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileVersionArn"]
-    GitHubRepositoryName     = "di-shared-assets"
-    OneLoginRepositoryName     = "shared-assets"
-    #TestImageRepositoryUri     = "913579054627.dkr.ecr.eu-west-2.amazonaws.com/test-repo-pipeline-imagerepository-ivqx0hq7tcyz"
-    #TestReportFormat           = "CUCUMBERJSON"
-    SlackNotificationType      = "Failures"
-    BuildNotificationStackName = "di-assets-notifications"
-  }
-
-  tags_custom = {
-    System = "DI ASSETS"
-  }
-}
-
-module "upload-event-rule-pipeline" {
-  source     = "git@github.com:alphagov/di-ipv-terraform-modules.git//secure-pipeline/deploy-pipeline"
-  stack_name = "upload-event-rule-pipeline"
-  parameters = {
-    SAMStackName             = "upload-event-rule"
-    Environment              = "build"
-    VpcStackName             = "vpc"
-    AllowedAccounts          = "791977510340"
-    # AWSOrganizationId        = data.aws_organizations_organization.gds.id
-    LogRetentionDays         = 7
-    SigningProfileArn        = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileArn"]
-    SigningProfileVersionArn = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileVersionArn"]
-    GitHubRepositoryName     = "di-shared-assets"
-    OneLoginRepositoryName     = "shared-assets"
-    #TestImageRepositoryUri     = "913579054627.dkr.ecr.eu-west-2.amazonaws.com/test-repo-pipeline-imagerepository-ivqx0hq7tcyz"
-    #TestReportFormat           = "CUCUMBERJSON"
-    SlackNotificationType      = "Failures"
-    BuildNotificationStackName = "di-assets-notifications"
-  }
-
-  tags_custom = {
-    System = "DI ASSETS"
+    System = "DevPlatform"
   }
 }
