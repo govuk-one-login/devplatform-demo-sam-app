b800b911f191b9927eedf9078f4ed7adaf411196
Phillip Du Plooy <phil.du-ploy@digital.cabinet-office.gov.uk>
2023-11-21 10:17:55 +0000

    Creating new pipeline

--- terraform/deployments/devplatform-development/pipeline-deploy/outputs.tf
+++ terraform/deployments/devplatform-development/pipeline-deploy/outputs.tf
@@ -11,3 +11,13 @@ output "service-catalog-pipeline_stack_tags" {
 output "gds_org_id" {
   value = data.aws_organizations_organization.gds.id
 }
+
+output "service-resource-pipeline_stack_id" {
+  value = module.service-resource-pipeline.stack_id
+}
+output "service-resource-pipeline_stack_outputs" {
+  value = module.service-resource-pipeline.stack_outputs
+}
+output "service-resource-pipeline_stack_tags" {
+  value = module.service-resource-pipeline.stack_tags
+}
--- terraform/deployments/devplatform-development/pipeline-deploy/stacks.tf
+++ terraform/deployments/devplatform-development/pipeline-deploy/stacks.tf
@@ -27,3 +27,26 @@ module "service-catalog-pipeline" {
     System = "DevPlatform"
   }
 }
+
+module "service-resource-pipeline" {
+  source     = "git@github.com:alphagov/di-ipv-terraform-modules.git//secure-pipeline/deploy-pipeline"
+  template_url = "https://template-bucket-templatebucket-35qbug5k1irh.s3.eu-west-2.amazonaws.com/test-templates/sam-deploy-pipeline/PLAT-2936-template.yaml"
+  stack_name = "service-resource-pipeline"
+  parameters = {
+    SAMStackName               = "service-resource-poc"
+    Environment                = "dev"
+    VpcStackName               = "vpc"
+    IncludePromotion           = "No"
+    # AWSOrganizationId          = data.aws_organizations_organization.gds.id
+    LogRetentionDays           = 7
+    SigningProfileArn          = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileArn"]
+    SigningProfileVersionArn   = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileVersionArn"]
+    OneLoginRepositoryName     = "devplatform-demo-sam-app"
+    SlackNotificationType      = "Failures"
+    BuildNotificationStackName = "build-notifications"
+  }
+
+  tags_custom = {
+    System = "DevPlatform"
+  }
+}
