d7cd67ce5786a1c631d07cf03694bd22497d3221
Phillip Du Plooy <phil.du-ploy@digital.cabinet-office.gov.uk>
2024-01-16 14:38:03 +0000

    Service Catalog setup

--- terraform/deployments/devplatform-development/account/site.tf
+++ terraform/deployments/devplatform-development/account/site.tf
@@ -3,7 +3,7 @@ terraform {
 
   # Comment out when bootstrapping
   backend "s3" {
-    bucket = "devplatform-service-catalog-tfstate"
+    bucket = "devplatform-development-tfstate"
     key    = "account.tfstate"
     region = "eu-west-2"
   }
@@ -15,12 +15,12 @@ provider "aws" {
 
 module "state_bucket" {
   source         = "git@github.com:govuk-one-login/ipv-terraform-modules//common/state-bucket-logging-tls"
-  bucket_name    = "devplatform-service-catalog-tfstate"
-  logging_bucket = "devplatform-service-catalog-access-logs"
+  bucket_name    = "devplatform-development-tfstate"
+  logging_bucket = "devplatform-development-access-logs"
   enable_tls     = true
 }
 module "logging_bucket" {
   source         = "git@github.com:govuk-one-login/ipv-terraform-modules//common/state-bucket-logging-tls"
-  bucket_name    = "devplatform-service-catalog-access-logs"
+  bucket_name    = "devplatform-development-access-logs"
   enable_tls     = true
 }
