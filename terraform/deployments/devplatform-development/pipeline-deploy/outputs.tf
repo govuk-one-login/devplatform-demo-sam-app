071d16c48817accefabc3b80e235c862b99a205a
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-01-23 15:58:43 +0000

    export cloudfront dist function and pipeline

--- terraform/deployments/devplatform-development/pipeline-deploy/outputs.tf
+++ terraform/deployments/devplatform-development/pipeline-deploy/outputs.tf
@@ -1,26 +1,26 @@
-output "service-catalog-pipeline_stack_id" {
-  value = module.service-catalog-pipeline.stack_id
-}
-output "service-catalog-pipeline_stack_outputs" {
-  value = module.service-catalog-pipeline.stack_outputs
-}
-output "service-catalog-pipeline_stack_tags" {
-  value = module.service-catalog-pipeline.stack_tags
-}
+#output "service-catalog-pipeline_stack_id" {
+#  value = module.service-catalog-pipeline.stack_id
+#}
+#output "service-catalog-pipeline_stack_outputs" {
+#  value = module.service-catalog-pipeline.stack_outputs
+#}
+#output "service-catalog-pipeline_stack_tags" {
+#  value = module.service-catalog-pipeline.stack_tags
+#}
 
 output "gds_org_id" {
   value = data.aws_organizations_organization.gds.id
 }
 
-output "service-resource-pipeline_stack_id" {
-  value = module.service-resource-pipeline.stack_id
-}
-output "service-resource-pipeline_stack_outputs" {
-  value = module.service-resource-pipeline.stack_outputs
-}
-output "service-resource-pipeline_stack_tags" {
-  value = module.service-resource-pipeline.stack_tags
-}
+#output "service-resource-pipeline_stack_id" {
+#  value = module.service-resource-pipeline.stack_id
+#}
+#output "service-resource-pipeline_stack_outputs" {
+#  value = module.service-resource-pipeline.stack_outputs
+#}
+#output "service-resource-pipeline_stack_tags" {
+#  value = module.service-resource-pipeline.stack_tags
+#}
 
 output "cloudfront-estimate-pipeline_stack_id" {
   value = module.cloudfront-estimate-pipeline.stack_id
--- terraform/deployments/devplatform-development/pipeline-deploy/stacks.tf
+++ terraform/deployments/devplatform-development/pipeline-deploy/stacks.tf
@@ -5,51 +5,51 @@ data "aws_cloudformation_stack" "aws-signer" {
   name = "aws-signer"
 }
 
-module "service-catalog-pipeline" {
-  source     = "git@github.com:govuk-one-login/ipv-terraform-modules//secure-pipeline/deploy-pipeline"
-  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.eu-west-2.amazonaws.com/sam-deploy-pipeline/template.yaml"
-  stack_name = "service-catalog-pipeline"
-  parameters = {
-    SAMStackName               = "service-catalog-poc"
-    Environment                = "dev"
-    VpcStackName               = "vpc-node-app"
-    IncludePromotion           = "No"
-    # AWSOrganizationId          = data.aws_organizations_organization.gds.id
-    LogRetentionDays           = 7
-    SigningProfileArn          = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileArn"]
-    SigningProfileVersionArn   = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileVersionArn"]
-    OneLoginRepositoryName     = "devplatform-demo-sam-app"
-    SlackNotificationType      = "Failures"
-    BuildNotificationStackName = "build-notifications"
-  }
+#module "service-catalog-pipeline" {
+#  source     = "git@github.com:govuk-one-login/ipv-terraform-modules//secure-pipeline/deploy-pipeline"
+#  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.eu-west-2.amazonaws.com/sam-deploy-pipeline/template.yaml"
+#  stack_name = "service-catalog-pipeline"
+#  parameters = {
+#    SAMStackName               = "service-catalog-poc"
+#    Environment                = "dev"
+#    VpcStackName               = "vpc-node-app"
+#    IncludePromotion           = "No"
+#    # AWSOrganizationId          = data.aws_organizations_organization.gds.id
+#    LogRetentionDays           = 7
+#    SigningProfileArn          = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileArn"]
+#    SigningProfileVersionArn   = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileVersionArn"]
+#    OneLoginRepositoryName     = "devplatform-demo-sam-app"
+#    SlackNotificationType      = "Failures"
+#    BuildNotificationStackName = "build-notifications"
+#  }
+#
+#  tags_custom = {
+#    System = "DevPlatform"
+#  }
+#}
 
-  tags_custom = {
-    System = "DevPlatform"
-  }
-}
-
-module "service-resource-pipeline" {
-  source     = "git@github.com:govuk-one-login/ipv-terraform-modules//secure-pipeline/deploy-pipeline"
-  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.eu-west-2.amazonaws.com/sam-deploy-pipeline/template.yaml"
-  stack_name = "service-resource-pipeline"
-  parameters = {
-    SAMStackName               = "service-resource-poc"
-    Environment                = "dev"
-    VpcStackName               = "vpc-node-app"
-    IncludePromotion           = "No"
-    # AWSOrganizationId          = data.aws_organizations_organization.gds.id
-    LogRetentionDays           = 7
-    SigningProfileArn          = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileArn"]
-    SigningProfileVersionArn   = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileVersionArn"]
-    OneLoginRepositoryName     = "devplatform-demo-sam-app"
-    SlackNotificationType      = "Failures"
-    BuildNotificationStackName = "build-notifications"
-  }
-
-  tags_custom = {
-    System = "DevPlatform"
-  }
-}
+#module "service-resource-pipeline" {
+#  source     = "git@github.com:govuk-one-login/ipv-terraform-modules//secure-pipeline/deploy-pipeline"
+#  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.eu-west-2.amazonaws.com/sam-deploy-pipeline/template.yaml"
+#  stack_name = "service-resource-pipeline"
+#  parameters = {
+#    SAMStackName               = "service-resource-poc"
+#    Environment                = "dev"
+#    VpcStackName               = "vpc-node-app"
+#    IncludePromotion           = "No"
+#    # AWSOrganizationId          = data.aws_organizations_organization.gds.id
+#    LogRetentionDays           = 7
+#    SigningProfileArn          = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileArn"]
+#    SigningProfileVersionArn   = data.aws_cloudformation_stack.aws-signer.outputs["SigningProfileVersionArn"]
+#    OneLoginRepositoryName     = "devplatform-demo-sam-app"
+#    SlackNotificationType      = "Failures"
+#    BuildNotificationStackName = "build-notifications"
+#  }
+#
+#  tags_custom = {
+#    System = "DevPlatform"
+#  }
+#}
 
 module "cloudfront-estimate-ecr" {
   source     = "git@github.com:govuk-one-login/ipv-terraform-modules.git//secure-pipeline/container-image-repository"
