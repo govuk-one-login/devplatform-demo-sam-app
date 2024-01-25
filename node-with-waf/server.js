62f5c46a296944ca9fc11e5b8f280acd92f50272
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-01-25 14:16:23 +0000

    export cloudfront dist function and pipeline

--- node-with-waf/server.js
+++ node-with-waf/server.js
@@ -3,9 +3,9 @@ const hostname = '0.0.0.0';
 const port = 8000;
 const server = http.createServer(function (request, response) {
     response.statusCode = 200;
-    response.setHeader("Content-Type", "text/plain" );
+    response.setHeader("Content-Type", "application/json" );
     const requestHeaders = JSON.stringify(request.headers);
-    response.end("Hello World\n" + requestHeaders);
+    response.end(requestHeaders);
 });
 
 server.listen(port, hostname, ()=> {
