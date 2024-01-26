7b91291b42b3a60f72d25979cd6ffcf324ae92ab
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-01-26 11:58:44 +0000

    export cloudfront dist function and pipeline

--- node-with-waf/server.js
+++ node-with-waf/server.js
@@ -4,12 +4,11 @@ const port = 8000;
 const server = http.createServer(function (request, response) {
     response.statusCode = 200;
     response.setHeader("Content-Type", "application/json" );
-    const requestHeaders = JSON.stringify(request.headers);
-    let txmaAuditDecoded = '';
     if(request.headers.hasOwnProperty('txma-audit-encoded')) {
-        txmaAuditDecoded = atob(request['headers']['txma-audit-encoded']);
+        request.headers.txmaAuditDecoded = atob(request['headers']['txma-audit-encoded']);
     }
-    response.end(requestHeaders + ',{"txmaAuditDecoded":' + txmaAuditDecoded + '}');
+    const requestHeaders = JSON.stringify(request.headers);
+    response.end(requestHeaders);
 });
 
 server.listen(port, hostname, ()=> {
