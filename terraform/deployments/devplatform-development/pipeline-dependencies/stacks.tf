99179396aab6983058d7667076c192f8aff22405
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-03-12 15:39:18 +0000

    fiddling around with domain names

--- terraform/deployments/devplatform-development/pipeline-dependencies/stacks.tf
+++ terraform/deployments/devplatform-development/pipeline-dependencies/stacks.tf
@@ -9,9 +9,8 @@ module "devplatform-cloudfront-distribution" {
     stack_name = "devplatform-cf-dist"
     parameters = {
         DistributionAlias = "cloudfront-estimate.dev.platform.sandpit.account.gov.uk"
-        DistributionCertificateArn = "arn:aws:acm:us-east-1:842766856468:certificate/ece18c99-4a54-42c2-8da6-5e0acd244f60"
-        OriginDomain = "1fu1mudkf6.execute-api.eu-west-2.amazonaws.com"
-        OriginPath = "/cloudfront-estimate-RestApiGatewayStage-301df360-aa32-11ee-bcd0-06a53b5f56c2"
+        HostedZoneID = "Z0523977PEWYQWQ6TINO"
+        AddWWWPrefix = "true"
     }
 
     tags_custom = {
