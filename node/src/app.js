267b9afd430e232a3d61c198b8d1c037adf4c130
nheal_deloitte <nheal@deloitte.com>
2024-03-19 16:31:56 +0000

    feat: add logging

--- node/src/app.js
+++ node/src/app.js
@@ -1,6 +1,7 @@
 require("express");
 require("express-async-errors");
 
+const os = require("os");
 const path = require("path");
 const session = require("express-session");
 const AWS = require("aws-sdk");
@@ -53,6 +54,9 @@ const sessionConfig = {
 
 const helmetConfig = require("ipv-cri-common-express/src/lib/helmet");
 
+let requests = [];
+let responseTimes = [];
+
 const { app, router } = setup({
   config: { APP_ROOT: __dirname },
   port: PORT,
@@ -78,6 +82,19 @@ const { app, router } = setup({
     cookie: { name: "lng" },
   },
   middlewareSetupFn: (app) => {
+    app.use((req, res, next) => {
+      requests.push({ timestamp: Date.now() });
+      const start = Date.now();
+      res.on("finish", () => {
+        const duration = Date.now() - start;
+        responseTimes.push({
+          timestamp: Date.now(),
+          duration,
+        });
+      });
+      next();
+    });
+
     app.use(setHeaders);
   },
   dev: true,
@@ -118,3 +135,49 @@ router.use("/oauth2", commonExpress.routes.oauth2);
 router.use("/toy", require("./app/toy"));
 
 router.use(commonExpress.lib.errorHandling.redirectAsErrorToCallback);
+
+// --------- test logging
+
+const getAverageResponseTime = (seconds = 20) => {
+  const start = new Date(Date.now() - seconds * 1000);
+  responseTimes = responseTimes.filter(({ timestamp }) => timestamp >= start);
+  return (
+    responseTimes
+      .map(({ duration }) => duration)
+      .reduce((total, current) => total + current, 0) / responseTimes.length
+  );
+};
+
+const getRequests = (seconds = 20) => {
+  const start = new Date(Date.now() - seconds * 1000);
+  requests = requests.filter(({ timestamp }) => timestamp >= start);
+  return requests.length;
+};
+
+const logStats = () => {
+  return {
+    event: "",
+    cpuUsage: `${os.loadavg()[0].toFixed(2)}%`,
+    memoryUsage:
+      (
+        (process.memoryUsage().heapUsed / process.memoryUsage().heapTotal) *
+        100
+      ).toFixed(2) + "%",
+    requestsLast20Seconds: getRequests(),
+    averageResponseTimeLast20Seconds: `${getAverageResponseTime()}ms`,
+  };
+};
+
+setInterval(() => {
+  console.log({
+    ...logStats(),
+    event: "monitoring",
+  });
+}, 20000);
+
+process.on("SIGTERM", () => {
+  console.log({
+    ...logStats(),
+    event: "shutdown-initiated",
+  });
+});
