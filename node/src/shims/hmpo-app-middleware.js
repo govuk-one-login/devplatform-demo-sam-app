fa3ade0aba0b759ea54e1018d020ed3ff3484b65
Andrew Loughran <andrew.loughran@digital.cabinet-office.gov.uk>
2024-04-23 14:02:18 +0100

    Fixed to move server to right bit of code

--- node/src/shims/hmpo-app-middleware.js
+++ node/src/shims/hmpo-app-middleware.js
@@ -1,10 +1,19 @@
 const path = require("path");
 const express = require("express");
 const logger = require("hmpo-app/lib/logger");
+const AWS = require("aws-sdk");
+// Set the region
+AWS.config.update({ region: "eu-west-2" });
 
 const requiredArgument = (argName) => {
   throw new Error(`Argument '${argName}' must be specified`);
 };
+
+// Create CloudWatch service object
+const cloudwatch = new AWS.CloudWatch({ apiVersion: "2010-08-01" });
+const os = require("os");
+const containerName = os.hostname() 
+const schedule = require('node-schedule');
 
 const middleware = {
   setup({
@@ -134,6 +143,47 @@ const middleware = {
 
     server.keepAliveTimeout = 65000;
     server.headersTimeout = 66000;
+
+    // Push count every seconds
+    schedule.scheduleJob("* * * * * *", () => {
+      return server.getConnections((error, count) => {
+        if (error) {
+          console.error("Error while trying to get server connections", error);
+          return;
+        }
+
+        console.log(`Current opened connections count: ${count}`);
+
+        const params = {
+          MetricData: [
+            {
+              MetricName: "HTTPConnections",
+              Dimensions: [
+                {
+                  Name: "PerNodeId",
+                  Value: `${containerName}`,
+                  // Set here any dynamic and unique ID
+                  // than can identify easily your running
+                  // node app, like its container ID
+                },
+              ],
+              Unit: "Count",
+              Value: count,
+            },
+          ],
+          Namespace: "FEC/NodeApp",
+        };
+        //Make sure to set the IAM policy to allow pushing metrics
+        cloudwatch.putMetricData(params, (err) => {
+          if (err) {
+            console.error(
+              "Error while trying to push http connections metrics",
+              err
+            );
+          }
+        });
+      });
+    });
   },
 };
 
