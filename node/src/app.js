1feb3735a53cee0cff40a32667610b7408e065b4
Andrew Loughran <andrew.loughran@digital.cabinet-office.gov.uk>
2024-04-23 12:58:28 +0100

    Fixing AWS errors

--- node/src/app.js
+++ node/src/app.js
@@ -4,11 +4,14 @@ require("express-async-errors");
 const path = require("path");
 const session = require("express-session");
 const AWS = require("aws-sdk");
+// Set the region
+AWS.config.update({ region: "eu-west-2" });
 const DynamoDBStore = require("connect-dynamodb")(session);
 // Create CloudWatch service object
 const cloudwatch = new AWS.CloudWatch({ apiVersion: "2010-08-01" });
 const os = require("os");
 const containerName = os.hostname() 
+const schedule = require('node-schedule');
 
 const commonExpress = require("ipv-cri-common-express");
 
