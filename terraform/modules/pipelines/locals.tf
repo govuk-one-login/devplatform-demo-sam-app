95f8ca4c984b864977c63522ba8dc3122f14e59f
Monjurul Haque <monjurul.haque@digital.cabinet-office.gov.uk>
2024-04-29 10:01:07 +0100

    Fix Environment tag in pipeline
    Also tidy up the build or dev conditionals using contains() function
    
    Issue: PLAT-568
    Co-authored-by:

--- terraform/modules/pipelines/locals.tf
+++ terraform/modules/pipelines/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  container_signer_kms_key_arn = var.environment == "build" || var.environment == "dev" ? data.aws_cloudformation_stack.container-signer[0].outputs["ContainerSignerKmsKeyArn"] : var.container_signer_kms_key_arn
-  signing_profile_arn          = var.environment == "build" || var.environment == "dev" ?  data.aws_cloudformation_stack.aws-signer[0].outputs["SigningProfileArn"] : var.signing_profile_arn
-  signing_profile_version_arn  = var.environment == "build" || var.environment == "dev" ?  data.aws_cloudformation_stack.aws-signer[0].outputs["SigningProfileVersionArn"] : var.signing_profile_version_arn
+  container_signer_kms_key_arn = contains(["dev", "build"], var.environment) ? data.aws_cloudformation_stack.container-signer[0].outputs["ContainerSignerKmsKeyArn"] : var.container_signer_kms_key_arn
+  signing_profile_arn          = contains(["dev", "build"], var.environment) ? data.aws_cloudformation_stack.aws-signer[0].outputs["SigningProfileArn"] : var.signing_profile_arn
+  signing_profile_version_arn  = contains(["dev", "build"], var.environment) ? data.aws_cloudformation_stack.aws-signer[0].outputs["SigningProfileVersionArn"] : var.signing_profile_version_arn
 }
