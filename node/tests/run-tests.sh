3bb7f2f091938cfb30ee28e410e3f168bd5070f2
Sandra Soinska <sandra.soinska@digital.cabinet-office.gov.uk>
2024-05-13 14:53:23 +0100

    PSREDP-146 test script for 200 status

--- node/tests/run-tests.sh
+++ node/tests/run-tests.sh
@@ -1,10 +1,12 @@
-set -eu
-URL=https://da0bkvp74e.execute-api.eu-west-2.amazonaws.com/healthcheck
+#!/usr/bin/env bash
 
-### This first test checks that the hello endpoint can serve up 20000 requests with a concurrency of 10 requests per second
-ab -n 20000 -c 10 $URL
+set -eu
 
 declare status_code
 # shellcheck disable=SC2154
-status_code="$(curl --silent --output /dev/null --write-out "%{http_code}" "$URL")"
-echo "$status_code"
+status_code="$(curl --silent --output /dev/null --write-out '%{http_code}' "$ApiGatewayEndpoint")"
+
+if [[ $status_code != "200" ]]; then
+exit 1
+else
+fi
