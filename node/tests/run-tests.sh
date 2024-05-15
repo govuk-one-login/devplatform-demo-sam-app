67122dd578495a80af95d3733dc414837f2d0cae
Sandra Soinska <sandra.soinska@digital.cabinet-office.gov.uk>
2024-05-15 16:30:01 +0100

    correcting syntax issue

--- node/tests/run-tests.sh
+++ node/tests/run-tests.sh
@@ -4,12 +4,10 @@ set -eu
 
 declare status_code
 # shellcheck disable=SC2154
-status_code="$(curl --silent --output /dev/null --write-out '%{http_code}' "https://www.bbc.co.uk/")"
+status_code="$(curl --silent --output /dev/null --write-out '%{http_code}' "$CFN_ApiGatewayEndpoint")"
 
-print("this is the status code:")
-print(status_code)
+print("this is the status code: $status_code")
 if [[ $status_code != "200" ]]
 then
     print("printing then statement")
-    print(status_code)
 fi
