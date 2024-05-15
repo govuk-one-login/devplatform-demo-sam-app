ffca1515526229b86d2cb0b2ed792bac627684f4
Sandra Soinska <sandra.soinska@digital.cabinet-office.gov.uk>
2024-05-15 16:52:20 +0100

    correcting syntax

--- node/tests/run-tests.sh
+++ node/tests/run-tests.sh
@@ -6,8 +6,7 @@ declare status_code
 # shellcheck disable=SC2154
 status_code="$(curl --silent --output /dev/null --write-out '%{http_code}' "$CFN_ApiGatewayEndpoint")"
 
-print("this is the status code: $status_code")
-if [[ $status_code != "200" ]]
-then
-    print("printing then statement")
+echo "this is the status code: $status_code"
+if [[ $status_code != "200" ]]; then
+exit 1
 fi
