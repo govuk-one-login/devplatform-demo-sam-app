982d5395eb013012149546fcbc3e757f3023965f
Sandra Soinska <sandra.soinska@digital.cabinet-office.gov.uk>
2024-05-15 16:08:33 +0100

    testing with bbc website

--- node/tests/run-tests.sh
+++ node/tests/run-tests.sh
@@ -4,7 +4,7 @@ set -eu
 
 declare status_code
 # shellcheck disable=SC2154
-status_code="$(curl --silent --output /dev/null --write-out '%{http_code}' "$CFN_ApiGatewayEndpoint")"
+status_code="$(curl --silent --output /dev/null --write-out '%{http_code}' "https://www.bbc.co.uk/")"
 
 print("this is the status code:")
 print(status_code)
