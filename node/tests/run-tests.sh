35befeb75f0605f9d9a7e47e9b425faa66615fee
Sandra Soinska <sandra.soinska@digital.cabinet-office.gov.uk>
2024-05-15 17:05:01 +0100

    added health check to url

--- node/tests/run-tests.sh
+++ node/tests/run-tests.sh
@@ -4,7 +4,7 @@ set -eu
 
 declare status_code
 # shellcheck disable=SC2154
-status_code="$(curl --silent --output /dev/null --write-out '%{http_code}' "$CFN_ApiGatewayEndpoint")"
+status_code="$(curl --silent --output /dev/null --write-out '%{http_code}' "$CFN_ApiGatewayEndpoint/healthcheck")"
 
 echo "this is the status code: $status_code"
 if [[ $status_code != "200" ]]; then
