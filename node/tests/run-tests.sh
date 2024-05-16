9380c0a4be137aaf287ff71e2791a8c8fe20d4f0
Sandra Soinska <sandra.soinska@digital.cabinet-office.gov.uk>
2024-05-16 13:33:40 +0100

    PSRED-146 updated node endpoint /healthcheck

--- node/tests/run-tests.sh
+++ node/tests/run-tests.sh
@@ -4,9 +4,8 @@ set -eu
 
 declare status_code
 # shellcheck disable=SC2154
-status_code="$(curl --silent --output /dev/null --write-out '%{http_code}' "$CFN_ApiGatewayEndpoint")"
+status_code="$(curl --silent --output /dev/null --write-out '%{http_code}' "$CFN_ApiGatewayEndpoint/healthcheck")"
 
 if [[ $status_code != "200" ]]; then
 exit 1
-else
 fi
