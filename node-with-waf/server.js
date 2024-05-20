2018b8e8549753d8bd3b5dfd479b337c0a0a8545
Zaynob Sumon <zaynobs@gmail.com>
2024-05-20 16:06:38 +0100

    changing body content if 502 alarm is triggered

--- node-with-waf/server.js
+++ node-with-waf/server.js
@@ -1,19 +1,24 @@
 const http = require('http');
 const hostname = '0.0.0.0';
 const port = 8000;
+
 const server = http.createServer(function (request, response) {
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "application/json" );
-    //if(request.headers.hasOwnProperty('txma-audit-encoded')) {
-        //request.headers.txmaAuditDecoded = atob(request['headers']['txma-audit-encoded']);
-    //}
-        if(request.url == '/giveme502'){
-        response.statusCode = 502
+    if (request.url === '/giveme502') {
+        response.statusCode = 502;
+        response.setHeader('Content-Type', 'application/json');
+        response.end(JSON.stringify({ error: 'Intentional 502 error: Bad Gateway' }));
+    } else {
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'application/json');
+        const requestHeaders = JSON.stringify(request.headers);
+        response.end(requestHeaders);
     }
-    const requestHeaders = JSON.stringify(request.headers);
-    response.end(requestHeaders);
 });
 
-server.listen(port, hostname, ()=> {
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+server.on('error', (error) => {
+    console.error('Server error:', error);
 });
