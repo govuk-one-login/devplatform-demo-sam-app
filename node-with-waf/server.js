897f832ce79fc020830a72b1e374c6a3802ee1e8
Zaynob Sumon <zaynobs@gmail.com>
2024-05-23 16:53:54 +0100

    updating 503 server unavailable alarms as a url path

--- node-with-waf/server.js
+++ node-with-waf/server.js
@@ -5,16 +5,30 @@ const port = 8000;
 let serverAvailable = true;
 
 const server = http.createServer(function (request, response) {
-    if (!serverAvailable) {
-        response.statusCode = 503;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify({ error: 'Service Unavailable' }));
-        return;
-    }
+    //503 error
+        if (request.url === '/giveme503') {
+            serverAvailable = false; // Turn off server availability
+            setTimeout(() => {
+                serverAvailable = true; // Turn on server availability after some time
+                console.log('Server is now available');
+            }, 60000); // Turn on server after 60 seconds
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'application/json');
+            response.end(JSON.stringify({ message: 'Server will be unavailable for 60 seconds' }));
+            return;
+        }
+        if (!serverAvailable) {
+            response.statusCode = 503;
+            response.setHeader('Content-Type', 'application/json');
+            response.end(JSON.stringify({ error: 'Service Unavailable' }));
+            return;
+        }
+    //502 error
     if (request.url === '/giveme502') {
         response.statusCode = 502;
         response.setHeader('Content-Type', 'application/json');
         response.end(JSON.stringify({ error: 'Intentional 502 error: Bad Gateway' }));
+    //504 error
     } else if (request.url === '/giveme504') {
         setTimeout(() => {
             response.statusCode = 504;
@@ -32,11 +46,6 @@ const server = http.createServer(function (request, response) {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-
 server.on('error', (error) => {
     console.error('Server error:', error);
 });
-setTimeout(() => {
-    serverAvailable = false;
-    console.log('Server is now unavailable (503)');
-}, 60000);
