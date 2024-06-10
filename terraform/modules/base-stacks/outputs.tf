f1bc7b2e6ee354804e3d61c516f647b2b6bc0c7a
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-06-10 09:00:57 +0100

    Removed terraform directory from repo
    Copied to devplatform-common-infra as having all the TF in one place is simpler and make it easier to apply

--- terraform/modules/base-stacks/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aws-signer_stack_outputs" {
-    value = contains(["dev", "build"], var.environment) ? module.aws-signer[0].stack_outputs : null
-}
-
-output "container-signer_stack_outputs" {
-    value = contains(["dev", "build"], var.environment) ? module.container-signer[0].stack_outputs : null
-}
