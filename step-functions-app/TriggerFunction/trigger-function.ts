00a063ec6e4728a4b9ac62d0001896cff6c6b66c
fpmrqs <fpottinger.marques@gmail.com>
2024-07-11 08:59:37 +0100

    PSREDEV-1101: added api gateway

--- step-functions-app/TriggerFunction/trigger-function.ts
+++ step-functions-app/TriggerFunction/trigger-function.ts
@@ -3,7 +3,17 @@ import { SFN } from '@aws-sdk/client-sfn';
 export const sfnClient = new SFN({ region: process.env.AWS_REGION });
 
 export const triggerHandler = async () => {
-    const stateMachineArn: string = process.env.STATE_MACHINE!
-    await sfnClient.startExecution({ stateMachineArn: stateMachineArn });
-    return 'done';
+    const triggerLambdaVersion = process.env.AWS_LAMBDA_FUNCTION_VERSION!
+    await sfnClient.startExecution( stateMachineTrigger(triggerLambdaVersion) );
+    const message: string = `Trigger lambda version ${ triggerLambdaVersion } started execution`;
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(message)
+    }
 };
+
+const stateMachineTrigger = ( triggerLambdaVersion: string ) => ({
+    stateMachineArn: process.env.STATE_MACHINE!,
+    input: JSON.stringify({ "triggerLambdaVersion": triggerLambdaVersion })
+})
