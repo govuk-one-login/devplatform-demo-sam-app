e5ac5f1a49c65e549944d7f956fa619cfe8ccd7d
Beca Galliano <beca.galliano@digital.cabinet-office.gov.uk>
2024-08-21 14:05:53 +0100

    PSREDEV-1061: Refactor server.js

--- node-with-waf/server.js
+++ node-with-waf/server.js
@@ -1,41 +1,37 @@
 const http = require('http');
+const url = require('url');
 const hostname = '0.0.0.0';
 const port = 8000;
 
-//boolan needed for 503 error
-let serverAvailable = true;
-
 const server = http.createServer(function (request, response) {
-    //503 error - will render the server inaccesible for 60 seconds. No calls will be accepted until time is up.
-        if (request.url === '/giveme503') {
-            serverAvailable = false; // Turn off server availability
+    const parsedURL = url.parse(
+        request.url,
+        true
+    );
+    let delay
+    let statusCode
+
+    if (parsedURL.pathname === '/testResponse') {
+        if (!isNaN(parsedURL.query.delay)) {
+            delay = parsedURL.query.delay;
+          } else {
+            delay = 0;
+          }
+
+          if (!isNaN(parsedURL.query.statusCode)) {
+            statusCode = parsedURL.query.statusCode;
+          } else {
+            statusCode = 200;
+          }
+
+        if ('delay' in parsedURL.query || 'statueCode' in parsedURL.query) {
             setTimeout(() => {
-                serverAvailable = true; // Turn on server availability after some time
-                console.log('Server is now available');
-            }, 60000); // Turn on server after 60 seconds
-            response.statusCode = 200; //after 60 seconds server will start accepting calls
-            response.setHeader('Content-Type', 'application/json');
-            response.end(JSON.stringify({ message: 'Server will be unavailable for 60 seconds' }));
+                response.statusCode = statusCode;
+                response.setHeader('Content-Type', 'application/json');
+                response.end(JSON.stringify({ error: `Intentional ${statusCode} error` }));
+            }, delay);
             return;
         }
-        if (!serverAvailable) {
-            response.statusCode = 503;
-            response.setHeader('Content-Type', 'application/json');
-            response.end(JSON.stringify({ error: 'Intentional 503: Service Unavailable' }));
-            return;
-        }
-    //502 error - Bad Gateway error
-    if (request.url === '/giveme502') {
-        response.statusCode = 502;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify({ error: 'Intentional 502: Bad Gateway' }));
-    //504 error - Gateway Timeout - 30 sec deley
-    } else if (request.url === '/giveme504') {
-        setTimeout(() => {
-            response.statusCode = 504;
-            response.setHeader('Content-Type', 'application/json');
-            response.end(JSON.stringify({ error: 'Intential 504: Gateway Timeout' }));
-        }, 30000);
     } else {
         response.statusCode = 200; //working state of the app - no endpoints
         response.setHeader('Content-Type', 'application/json');
